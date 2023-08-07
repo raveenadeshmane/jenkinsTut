1be3b889458640ddbbe8a17dec9dd858fda2520a
Raveena Deshmane <rdeshmane@eisgroup.com>
2023-08-07 10:52:28 +0300

    main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String str = "Job is working " + new Random().nextInt();
+        FileOutputStream outputStream = new FileOutputStream("OpenL.txt");
+        byte[] strToBytes = str.getBytes();
+        outputStream.write(strToBytes);
+
+        outputStream.close();
         System.out.println("Job is working ");
     }
 }
