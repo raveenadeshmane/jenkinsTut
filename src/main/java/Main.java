8ed989682fbf62bfa7edeae2df4e80c55724ca03
Raveena Deshmane <rdeshmane@eisgroup.com>
2023-08-07 12:13:02 +0300

    main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String str = "Job is working " + new Random().nextInt();
         System.out.println("String is: " + str);
-        File file =new File(Paths.get("").toAbsolutePath()+File.separator + "OpenL.txt");
+        File file =new File(System.getProperty("user.dir")+File.separator + "OpenL.txt");
         if (!file.exists()) {
             file.createNewFile();
         }
